431aeda0d95136adeaa4a644dff6359a5371c772
thomasvanvelzen <s1113113@student.hsleiden.nl>
2023-01-12 21:21:10 +0100

    Update db details

--- src/connection/typeorm-datasource.ts
+++ src/connection/typeorm-datasource.ts
@@ -2,16 +2,16 @@ import { createConnections, DataSource, DataSourceOptions } from 'typeorm';
 
 const defaultOptions: DataSourceOptions = {
     type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: '1234',
+    host: 'mysql-3de73fdc-thovv83-f6ea.aivencloud.com',
+    port: 10700,
+    username: 'avnadmin',
+    password: 'AVNS_4NDX-EI9M2WthKyv2kh',
 };
 //
 
 export const dataSource: DataSource = new DataSource({
     ...defaultOptions,
-    database: 'webshop',
+    database: 'defaultdb',
 });
 
 dataSource.initialize();
